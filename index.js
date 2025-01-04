452fc9b78a3ea506000b8eaeb655c332f23bf2e4
mehedi-hasannnn <mhasan2999@gmail.com>
2025-01-04 23:44:58 +0600

    added cart collection function

--- index.js
+++ index.js
@@ -28,10 +28,24 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
 
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get('/reviews', async(req,res)=>{
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    })
+
+    // carts collection
+    app.post('/carts', async(req, res)=>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
